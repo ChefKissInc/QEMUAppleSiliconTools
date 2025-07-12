fd08782df287a888afa575320795852184fef4be
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2025-07-12 13:55:30 +0300

    refactor: Slight patch comment adjustments

--- PatchDYLD.sh
+++ PatchDYLD.sh
@@ -11,12 +11,12 @@ echo "Restoring original DYLD..."
 dd if=dyld_shared_cache_arm64e.orig of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying CoreImage patches..."
-# Force `wrapGLIsUsable` to return false
+# Force `wrapGLIsUsable` to return false.
 printf '\x00\x00\x80\xD2' | dd bs=1 count=4 seek=32044804 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=32044808 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying QuartzCore patches..."
-# Fix `CA::OGL::AsynchronousDispatcher::renderer` to use software context
+# Fix `CA::OGL::AsynchronousDispatcher::renderer` to use software context.
 printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542256 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542260 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542264 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
@@ -25,6 +25,6 @@ printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542368 conv=notrunc of=/Volu
 printf '\x1F\x20\x03\xD5' | dd bs=1 count=4 seek=135542372 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 
 echo "Applying SpringBoardFoundation.framework patches..."
-# Force `shouldUseXPCForRendering` to return true
+# Force `shouldUseXPCForRendering` to return true.
 printf '\x20\x00\x80\xD2' | dd bs=1 count=4 seek=367611672 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
 printf '\xC0\x03\x5F\xD6' | dd bs=1 count=4 seek=367611676 conv=notrunc of=/Volumes/System/System/Library/Caches/com.apple.dyld/dyld_shared_cache_arm64e
